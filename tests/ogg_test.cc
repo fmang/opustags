4d9ee3bf880af851f596fbb698cc22bf4079bb61
Frédéric Mangano <frederic.mangano@u-psud.fr>
2016-03-01 15:49:04 +0100

    ogg decoding test, along with an opus sample

---
+++ tests/ogg_test.cc
@@ -0,0 +1,25 @@
+#include "ogg.h"
+#include "catch.h"
+
+#include <fstream>
+
+using namespace opustags;
+
+SCENARIO("decoding a single-stream file", "[ogg]")
+{
+
+    GIVEN("an ogg::Decoder") {
+        std::ifstream src("../tests/samples/mystery.ogg");
+        ogg::Decoder dec(src);
+
+        WHEN("reading the first page") {
+            ogg::Stream *s = dec.read_page();
+            THEN("the Opus header is ready") {
+                REQUIRE(s != nullptr);
+                REQUIRE(s->state == ogg::HEADER_READY);
+                REQUIRE(s->type == ogg::OPUS_STREAM);
+            }
+        }
+    }
+
+}
