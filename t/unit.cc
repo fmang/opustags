af988efd8a5167ab2dc0700c5402a140e931b229
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2018-11-04 18:24:29 -0500

    configure cmake for unit tests

---
+++ t/unit.cc
@@ -0,0 +1,17 @@
+/**
+ * \file t/unit.cc
+ *
+ * Entry point of the unit test suite.
+ * Follows the TAP protocol.
+ *
+ */
+
+#include <opustags.h>
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "1..1\nok - trivial\n";
+	return 0;
+}
